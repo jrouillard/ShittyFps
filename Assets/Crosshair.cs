a013d6de44d674238d9f325d576d84a00031247d
Rouillard <jon@rouillard.org>
2021-01-07 02:38:37 +0100

    Add Crosshair

---
+++ Assets/Crosshair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+    public GameObject cross;
+    
+    public Transform maincamera;
+    private RaycastHit hit;
+    
+    // Start is called before the first frame update
+    void Start() {
+        cross.gameObject.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update() {
+        if (Physics.Raycast(maincamera.position, maincamera.forward, 300)) {
+            cross.GetComponent<Image>().material.SetColor("_Color", Color.red);
+        } else {
+            cross.GetComponent<Image>().material.SetColor("_Color", Color.white);
+        }
+    }
+}
